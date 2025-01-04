e45af817e3e13ca050b913e57885b4710b0c0109
Ahmad Khaled <Ahmads20002000@gmail.com>
2025-01-04 19:54:00 +0200

    Update Reservation class

--- HotelReservationSystem/Models/ReservationManagement/Reservation.cs
+++ HotelReservationSystem/Models/ReservationManagement/Reservation.cs
@@ -1,19 +1,18 @@
+using HotelReservationSystem.Data.Enums;
 using HotelReservationSystem.Models.PaymentManagement;
 
 namespace HotelReservationSystem.Models.ReservationManagement;
 
 public class Reservation : BaseModel
 {
-    public int ReservationID { get; set; }
-    public string GuestName { get; set; }
-    public string ReservationStatus { get; set; }
+    public ReservationStatus ReservationStatus { get; set; }
     public int NumberOfGuests { get; set; }
-    public string SpecialRequests { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public DateTime ModifiedDate { get; set; }
-    public string ContactDetails { get; set; }
+    public string SpecialRequests { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
     public decimal TotalAmount { get; set; }
     public decimal TotalAmountPaid { get; set; }
+    public int GuestId { get; set; }
     public ICollection<ReservationRoom> ReservationRooms { get; set; }
     public ICollection<Payment> Payments { get; set; }
 }
