a6668277d01064b299424cd0aa819fa44f5ad26d
adel <adel.fantasy11@gmail.com>
2025-01-12 21:42:07 +0200

    v-slicing

--- HotelReservationSystem/Data/Context.cs
+++ HotelReservationSystem/Data/Context.cs
@@ -1,4 +1,7 @@
-﻿using HotelReservationSystem.Models.OfferManagement;
+﻿using HotelReservationSystem.Models.GuestManagement;
+using HotelReservationSystem.Models.OfferManagement;
+using HotelReservationSystem.Models.PaymentManagement;
+using HotelReservationSystem.Models.ReservationManagement;
 using HotelReservationSystem.Models.RoomManagement;
 using HotelReservationSystem.Models.UserManagement;
 using Microsoft.EntityFrameworkCore;
@@ -21,12 +24,22 @@ public class Context : DbContext
     public DbSet<RoleFeatures> RoleFeatures { get; set; }
 
     // Rooms Management
-    public DbSet<Room> rooms { get; set; }
+    public DbSet<Room> Rooms { get; set; }
     public DbSet<Facility> Facilities { get; set; }
     public DbSet<RoomType> RoomTypes { get; set; }
     public DbSet<RoomFacility> RoomFacilities { get; set; }
     public DbSet<RoomTypeFacility> RoomTypeFacilities { get; set; }
 
+    // Reservation Management
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<ReservationRoom> ReservationRooms { get; set; }
+    
+    // Guest Management
+    public DbSet<Guest> Guests { get; set; }
+    
+    // Payment Management
+    public DbSet<Payment> Payments { get; set; }
+    
     // Offers Management
     public DbSet<Offer> Offers { get; set; }
 
